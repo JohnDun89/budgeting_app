382bf7a91dbedc3e7c4e004d68eb49c08a5e733c
John Duncan <john@johnduncanphoto.com>
2017-10-21 15:44:15 +0100

    tag save and show all methods added

--- models/tag.rb
+++ models/tag.rb
@@ -4,8 +4,31 @@ class Tag
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
-    @name = options 
+    @name = options
   end
 
+  def save
+    sql = "
+    INSERT INTO tag
+    (
+    name
+    )
+    VALUES
+    (
+    1$
+    )
+    RETURNING id
+    "
+    values = [@name]
+    results = SqlRunner.new (sql, values)
+    @id = results.first['id'].to_i
+  end
+
+  def self.all
+    sql = "SELECT * FROM tags"
+    values = []
+    results = SqlRunner.run( sql, values )
+    return results.map {|tag| Tag.new(tag)}
+  end
 
 end
